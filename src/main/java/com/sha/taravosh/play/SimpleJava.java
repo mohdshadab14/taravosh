47adf931e924b18f9558482b82ad50fb7146a882
Shadab <mohd.shadab14@gmail.com>
2026-08-18 14:33:15 +0530

    test file added for learning

---
+++ src/main/java/com/sha/taravosh/play/SimpleJava.java
@@ -0,0 +1,30 @@
+package com.sha.taravosh.play;
+
+public class SimpleJava {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Java");
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a+b);
+
+        /*
+        int
+        long
+        char
+        float
+        double
+        boolean - true/false
+        */
+
+        boolean likeJava = true;
+
+
+    }
+
+
+
+}
