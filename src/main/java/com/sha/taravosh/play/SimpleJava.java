a81b80184a7dda6e8c71a2e407d493efa3161ded
Shadab <mohd.shadab14@gmail.com>
2026-08-20 14:33:11 +0530

    java objects practice

--- src/main/java/com/sha/taravosh/play/SimpleJava.java
+++ src/main/java/com/sha/taravosh/play/SimpleJava.java
@@ -4,27 +4,25 @@ public class SimpleJava {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Java");
+        User user1 = new User("Shadab");
 
-        int a = 10;
-        int b = 20;
+        User user2 = new User("xyz");
 
-        System.out.println(a+b);
-
-        /*
-        int
-        long
-        char
-        float
-        double
-        boolean - true/false
-        */
-
-        boolean likeJava = true;
+        //user2.name = "some name";
 
 
+
+        System.out.println("User 1 Name is "+user1.name);
+        System.out.println("User 2 Name is "+user2.name);
+
+        int[] b = new int[100];
+        b[0] = 1;
+        b[5] = 100;
+
+        for(int i=0;i<100;i++){
+            //System.out.println("Value of array b index"+ i+ "is "+ b[i]);
+        }
     }
-
 
 
 }
---
+++ src/main/java/com/sha/taravosh/play/User.java
@@ -0,0 +1,11 @@
+package com.sha.taravosh.play;
+
+public class User {
+
+    String name;
+
+    User(String name){
+        this.name = name;
+    }
+}
+
