9cc0ec17cb7e74c94a74c1278236cc5df9eeed92
basha2385 <basha2385@gmail.com>
2026-08-20 14:48:15 +0530

    nowheera changes v1

--- src/main/java/com/sha/taravosh/play/SimpleJava.java
+++ src/main/java/com/sha/taravosh/play/SimpleJava.java
@@ -7,6 +7,8 @@ public class SimpleJava {
         User user1 = new User("Shadab");
 
         User user2 = new User("xyz");
+        User user3 = new User("Testing");
+
 
         //user2.name = "some name";
 
@@ -14,7 +16,7 @@ public class SimpleJava {
 
         System.out.println("User 1 Name is "+user1.name);
         System.out.println("User 2 Name is "+user2.name);
-
+        System.out.println("User 3 name is "+user3.name);
         int[] b = new int[100];
         b[0] = 1;
         b[5] = 100;
