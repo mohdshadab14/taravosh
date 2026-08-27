fa0d2ae81e83065a666069d82ed4ac86bc6f8a7c
akbar-nowheera <akbar.nowheera@gmail.com>
2026-08-27 14:12:44 +0530

    the class and object and constructor is explaind

--- src/main/java/com/sha/taravosh/assignments/assignment4_student_marks/Main.java
+++ src/main/java/com/sha/taravosh/assignments/assignment4_student_marks/Main.java
@@ -6,13 +6,14 @@ public class Main {
     public static void main(String[] args) {
 
         // TODO: Create a Student object using 'new'
-
-//               Student name = Rahul
-//               Marks per subject = 80
-            Student rahul = new Student("Rahul", 80);
-
+            Student rahul = new Student("Rahul");
+            rahul.marksEnglish = 80;
+            rahul.marksFrench = 60;
+            rahul.marksHindi = 80;
+            rahul.marksMath =100;
+            rahul.marksSci = 95;
         // TODO: Print student name
-        System.out.println(rahul);
+        System.out.println(rahul.calculateTotalMarks());
         // TODO: Call calculateTotalMarks()
         //       and print the result
     }
