13e6e8dd4a8455a6716e4c9624a894a0ebe29b16
akbar-nowheera <akbar.nowheera@gmail.com>
2026-09-01 10:19:10 +0530

    submitting assig3

--- src/main/java/com/sha/taravosh/assignments/assignment6_inheritance/assgn3/Main.java
+++ src/main/java/com/sha/taravosh/assignments/assignment6_inheritance/assgn3/Main.java
@@ -3,10 +3,14 @@ package com.sha.taravosh.assignments.assignment6_inheritance.assgn3;
 public class Main {
     public static void main(String[] args) {
         // TODO: Create a SavingsAccount object
+        SavingsAccount savingsAccount = new SavingsAccount("SV001",10000,5);
         // TODO: Display savings account details
+        System.out.println("Savings Account details ----------");
+        savingsAccount.displaySavingsDetails();
         // TODO: Create a CurrentAccount object
         CurrentAccount currentAccount = new CurrentAccount("AC001",10000, 5000);
         // TODO: Display current account details
+        System.out.println("Current Account details ----------");
         currentAccount.displayCurrentAccountDetails();
 
     }
--- src/main/java/com/sha/taravosh/assignments/assignment6_inheritance/assgn3/SavingsAccount.java
+++ src/main/java/com/sha/taravosh/assignments/assignment6_inheritance/assgn3/SavingsAccount.java
@@ -1,13 +1,19 @@
-//package com.sha.taravosh.assignments.assignment6_inheritance.assgn3;
-//
-//public class SavingsAccount extends BankAccount {
-//    double interestRate;
-//    // TODO: Create constructor
-//    public void displaySavingsDetails() {
-//        // TODO:
-//        // Call the inherited displayAccountDetails() method
-//        // TODO:
-//        // Print interest rate
-//    }
-//}
-//
+package com.sha.taravosh.assignments.assignment6_inheritance.assgn3;
+
+public class SavingsAccount extends BankAccount {
+    double interestRate;
+    // TODO: Create constructor
+    SavingsAccount(String AccountNumber, double balance, double interestRate){
+        super(AccountNumber,balance);
+        this.interestRate = interestRate;
+    }
+    public void displaySavingsDetails() {
+        // TODO:
+        // Call the inherited displayAccountDetails() method
+        displayAccountDetails();
+        // TODO:
+        // Print interest rate
+        System.out.println("Interest Rate : "+ interestRate);
+    }
+}
+
