8f1b78d63420d7b69291b523a66c404ace84aa18
Shadab <mohd.shadab14@gmail.com>
2026-09-02 13:40:39 +0530

    collection practice

---
+++ src/main/java/com/sha/taravosh/collection/StudentManager.java
@@ -0,0 +1,33 @@
+package com.sha.taravosh.collection;
+
+
+import java.util.ArrayList;
+
+public class StudentManager {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> students = new ArrayList<>();
+
+        // TODO 1: Add 5 student names to the list
+
+
+        // TODO 2: Print all students using an enhanced for loop
+
+
+        // TODO 3: Print the total number of students
+
+
+        // TODO 4: Remove one student from the list
+
+
+        // TODO 5: Print the list after removing the student
+
+
+        // TODO 6: Print the first student in the list
+
+
+        // TODO 7: Print all students using a normal for loop
+
+    }
+}
